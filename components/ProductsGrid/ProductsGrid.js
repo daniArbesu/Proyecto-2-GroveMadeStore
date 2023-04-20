310c0bce6b22b36b20250d56905991c232a552cc
daniArbesu <daniel.arbesu.sanchez@gmail.com>
2023-04-20 23:09:44 +0200

    fix: product grid

--- components/ProductsGrid/ProductsGrid.js
+++ components/ProductsGrid/ProductsGrid.js
@@ -4,8 +4,10 @@ import products from '../../utils/products';
 const template = () => {
   return ` 
   <h3>The Walnut Desk Collection</h3>
-  ${products.map(
-    ({ name, price, image, imageHover }) => `
+  <section id="products-grid" class="products-grid">
+  ${products
+    .map(
+      ({ name, price, image, imageHover }) => `
       <article class="shop-product">
                   <a
                     href="#"
@@ -30,37 +32,14 @@ const template = () => {
                   </a>
                 </article>
           `
-  )}`;
+    )
+    .join('')}
+    </section>
+    `;
 };
 
-/* const template = `
-<article class="shop-product">
-            <a
-              href="#"
-              rel="nofollow noopener noreferrer"
-            >
-              <figure class="shop-product__img">
-                <img
-                  src="https://grovemade.imgix.net/https%3A%2F%2Fgrovemade.com%2Fshop-static%2Fshop%2Fvariant%2Fwalnut-magsafe-dock-grid-A2.jpg%3F_v%3D1620162209.66?auto=format&ixlib=python-1.1.2&w=900&s=33560c81386dedc0016f6fd1b799c781"
-                  alt="Walnut MagSafe Stand"
-                  class="shop-product-main"
-                />
-                <img
-                  src="https://grovemade.imgix.net/https%3A%2F%2Fgrovemade.com%2Fshop-static%2Fshop%2Fvariant%2Fwalnut-magsafe-dock-grid-B2.jpg%3F_v%3D1620162209.66?auto=format&ixlib=python-1.1.2&w=900&s=ce6d616652b5986325b69264f3aee881"
-                  alt="Walnut MagSafe Stand Alternative"
-                  class="shop-product-alternative"
-                />
-              </figure>
-              <div class="shop-product__description">
-                <h4>Walnut MagSafe Stand</h4>
-                <p>$155</p>
-              </div>
-            </a>
-          </article>
-    `;
- */
 const printProductsGrid = () => {
-  document.querySelector('#products-grid').innerHTML = template();
+  document.querySelector('#products').innerHTML = template();
 };
 
 export default printProductsGrid;
