0dc908127c6bd4cbd3d57b8034fb5cc4d406c3aa
daniArbesu <daniel.arbesu.sanchez@gmail.com>
2023-04-26 19:54:13 +0200

    feat: add display and toggle to filter menu

--- components/Filter/Filter.js
+++ components/Filter/Filter.js
@@ -4,8 +4,8 @@ import printProductsGrid from '../ProductsGrid/ProductsGrid';
 
 const template = `
 <span>FILTER</span>
-<img class="filter-bar__x" src="https://grovemade.com/static/img/icons/x.svg?_v=1500009016.84" alt="Close button" aria-label="Close Filter">
-<div class="filter__menu-mobile">
+<img class="filter-bar__x" id="filter-bar__x"src="https://grovemade.com/static/img/icons/x.svg?_v=1500009016.84" alt="Close button" aria-label="Close Filter">
+<div class="filter__menu-mobile" id="filter__menu-mobile">
 <select id="seller-select">
   ${sellers.map((seller) => `<option value=${seller} >${seller}</option>`)}
 </select>
@@ -18,10 +18,20 @@ const template = `
     `;
 
 const listeners = () => {
+  const filterBar = document.querySelector('#filter-bar__x');
+  const filterMenuMobile = document.querySelector('#filter__menu-mobile');
   const select = document.querySelector('#seller-select');
   const maxpriceInput = document.querySelector('#maxprice-filter');
   const filterButton = document.querySelector('#filter-search');
   const clearFiltersButton = document.querySelector('#clean-filter');
+
+  filterBar.onclick = () => {
+    if (filterMenuMobile.style.display === 'none') {
+      filterMenuMobile.style.display = 'block';
+    } else {
+      filterMenuMobile.style.display = 'none';
+    }
+  };
 
   select.onchange = (e) => {
     const filteredSeller = e.target.value;
